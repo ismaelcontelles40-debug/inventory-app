d6ca47bd5d97978859dbc0922a70c3157ef03c2a
ismael <ismaelcontelles40@gmail.com>
2026-05-27 09:58:46 +0200

    fix render port production

--- backend/index.js
+++ backend/index.js
@@ -106,7 +106,7 @@ app.delete("/products/:id", async (req, res) => {
 // =====================
 // SERVER (IMPORTANTE PARA RENDER)
 // =====================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en puerto ${PORT}`);
